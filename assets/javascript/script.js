7e9b12fd879431d83edf61ab46b0aa4798c67942
Georgina Carlisle <georgina.carlisle@gmail.com>
2023-06-24 13:50:23 +0100

    Add function to handle users name and question

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,15 @@
-alert('JavaScript connected and running');
+let form = document.getElementById('ask-eightball');
+form.addEventListener('submit', handleQuestion);
+
+function handleQuestion(event) {
+    event.preventDefault();
+    let user = document.getElementById("name").value;
+    let question = document.getElementById("question").value;
+    if (user) {
+        document.getElementById('insert-name').textContent = user; 
+        } else {
+        document.getElementById('insert-name').textContent = "Stranger"; 
+        }
+    document.getElementById('insert-question').textContent = question; 
+}
+
